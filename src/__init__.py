230e5d1fe88245d2e41735d8b1c02a35d5b9ded7
Diego Quezada <heydiegoq@gmail.com>
2026-03-16 23:35:15 -0300

    add __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""Local Feature Store - A minimal feature store implementation."""
+
+from src.entity import Entity
+from src.feature import Feature
+from src.feature_store import FeatureStore
+
+__all__ = ["Entity", "Feature", "FeatureStore"]
